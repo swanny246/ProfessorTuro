ecc4a9c43bc4602746612380a978ec6e99639d2d
swanny246 <github@andrewswan.net>
2023-04-23 21:38:29 +1000

    Turo will handle AFKs correctly, condensed code

--- ProfessorTuro.py
+++ ProfessorTuro.py
@@ -1,5 +1,4 @@
-### Professor Turo bot
-### Created by evad3r
+## PROFESSOR TURO BOT
 
 import re, os, asyncio, random, string
 from discord.ext import commands, tasks
@@ -7,7 +6,7 @@ from pathlib import Path
 import json
 from json import loads
 
-version = 'v1.0'
+version = 'v1.1'
 
 ## Defining the server and bot token
 config = loads(Path("config.json").read_text())
@@ -19,7 +18,6 @@ spawns_2 = config["spawns_2"]
 spawns_3 = config["spawns_3"]
 spawns_4 = config["spawns_4"]
 spawns_5 = config["spawns_5"]
-incense = config["incense"]
 test_p2 = config["test_p2"]
 
 
@@ -45,85 +43,31 @@ async def on_ready():
     spam.start()
 
 ## Look for Pokename shiny hunt
-### Spawns 1
+
+async def handle_spawn_message(channel_id, content):
+    channel = client.get_channel(channel_id)
+    if 'Rare Ping' in content:
+        print(f'Rare ping in {channel.name}!')
+        await channel.send(f'?lock <#{channel_id}>')
+    elif 'Shiny Hunt Pings' in content and "@" in content:
+        print(f'Shiny Hunt detected in {channel.name}!')
+        await channel.send('?shlock')
+
+
 @client.event
 async def on_message(message):
-    if message.channel.id == int(spawns_1):
-        channel = client.get_channel(int(spawns_1))
-        if message.author.id == pokename:
-            if 'Rare Ping' in message.content:
-                print('Rare ping in spawns-1!')
-                channel = client.get_channel(int(spawns_1))
-                await channel.send('?lock '(int(spawns_1)))
-            elif 'Shiny Hunt Pings' in message.content:
-                print('Shiny Hunt detected in spawns-1!')
-                channel = client.get_channel(int(spawns_1))
-                await channel.send('?shlock')
-            
-    if message.channel.id == int(spawns_2):
-        channel = client.get_channel(int(spawns_2))
-        if 'Rare Ping' in message.content:
-                print('Rare ping in spawns-2!')
-                channel = client.get_channel(int(spawns_2))
-                await channel.send('?lock '(int(spawns_2)))
-        elif message.author.id == pokename:
-            if 'Shiny Hunt Pings' in message.content:
-                print('Shiny Hunt detected in spawns 2!')
-                channel = client.get_channel(int(spawns_2))
-                await channel.send('?shlock')
-        
-    if message.channel.id == int(spawns_3):
-        channel = client.get_channel(int(spawns_3))
-        if message.author.id == pokename:
-            if 'Rare Ping' in message.content:
-                print('Rare ping in spawns-3!')
-                channel = client.get_channel(int(spawns_3))
-                await channel.send('?lock '(int(spawns_3)))
-            elif 'Shiny Hunt Pings' in message.content:
-                print('Shiny Hunt detected in spawns 3!')
-                channel = client.get_channel(int(spawns_3))
-                await channel.send('?shlock')
-            
-    if message.channel.id == int(spawns_4):
-        channel = client.get_channel(int(spawns_4))
-        if message.author.id == pokename:
-            if 'Rare Ping' in message.content:
-                print('Rare ping in spawns-4!')
-                channel = client.get_channel(int(spawns_4))
-                await channel.send('?lock '(int(spawns_4)))
-            elif 'Shiny Hunt Pings' in message.content:
-                print('Shiny Hunt detected in spawns 4!')
-                channel = client.get_channel(int(spawns_4))
-                await channel.send('?shlock')
-            
-    if message.channel.id == int(spawns_5):
-        if message.author.id == pokename:
-            if 'Rare Ping' in message.content:
-                print('Rare ping in spawns-5!')
-                channel = client.get_channel(int(spawns_5))
-                await channel.send('?lock '(int(spawns_5)))
-            elif 'Shiny Hunt Pings' in message.content:
-                print('Shiny Hunt detected in spawns 5!')
-                channel = client.get_channel(int(spawns_5))
-                await channel.send('?shlock')
-            
-    if message.channel.id == int(incense):
-        if message.author.id == pokename:
-            if 'Rare Ping' in message.content:
-                print('Rare ping in incense!')
-            elif 'Shiny Hunt Pings' in message.content:
-                print('Shiny Hunt detected in incense!')
+    if message.author.id == pokename:
+        if message.channel.id == int(spawns_1):
+            await handle_spawn_message(int(spawns_1), message.content)
+        elif message.channel.id == int(spawns_2):
+            await handle_spawn_message(int(spawns_2), message.content)
+        elif message.channel.id == int(spawns_3):
+            await handle_spawn_message(int(spawns_3), message.content)
+        elif message.channel.id == int(spawns_4):
+            await handle_spawn_message(int(spawns_4), message.content)
+        elif message.channel.id == int(spawns_5):
+            await handle_spawn_message(int(spawns_5), message.content)
+        elif message.channel.id == int(test_p2):
+            await handle_spawn_message(int(test_p2), message.content)
 
-    if message.channel.id == int(test_p2):
-        if message.author.id == pokename:
-            if 'Rare Ping' in message.content:
-                print('Rare ping in test-p2!')
-                channel = client.get_channel(int(test_p2))
-                await channel.send(f'?lock <#{test_p2}>')
-            elif 'Shiny Hunt Pings' in message.content:
-                print('Shiny Hunt detected in spawns 5!')
-                channel = client.get_channel(int(test_p2))
-                await channel.send('?shlock')
-
-print(f'Pokétwo Autocatcher {version}\nA free and open-source Pokétwo autocatcher Modified by Viwes Bot\nEvent Log:')
 asyncio.run(client.run(f"{user_token}"))
